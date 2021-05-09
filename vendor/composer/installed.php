ed8be931c059c94424681ba8adb404a8d00cfaee
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-05-09 11:30:22 +0530

    adding composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '49f07e7d446241ab74ca233c5674a92a27233392',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '49f07e7d446241ab74ca233c5674a92a27233392',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.13.2',
+      'version' => '1.13.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+    ),
+  ),
+);
