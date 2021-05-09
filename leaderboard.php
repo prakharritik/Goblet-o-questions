e48fe3b649339b44ba7ef2605a6c21c9b4e1b945
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-05-09 11:37:50 +0530

    added title

--- leaderboard.php
+++ leaderboard.php
@@ -11,7 +11,7 @@
 ?>
 <!DOCTYPE html>
 <html>
-<head>
+<head><title>Goblet 'O' Questions</title>
  <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css">
--- quiz.php
+++ quiz.php
@@ -149,7 +149,7 @@ $rows=array(
         ?>
 <html>
     <head>
-        <title>E-Svadhyaya/Test</title>
+        <title>Goblet 'O' Questions</title>
 		<meta charset='utf-8'>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
